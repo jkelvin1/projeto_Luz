9990cbfc3dcd7031a00094b7a6be8c8ee4b4f8a3
John Gabriel <john.gabriel@fatecitapetininga.edu.br>
2021-05-12 15:18:23 -0300

    UPDATE 12/05

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,8 +1,11 @@
+using CRUD_ProdFinanceiro.BDCRUD;
+using CRUD_ProdFinanceiro.Connection;
 using CRUD_ProdFinanceiro.Interface;
 using CRUD_ProdFinanceiro.ViewModel;
 using Moq;
+using MySqlConnector;
 using NUnit.Framework;
-
+using System.Data;
 
 namespace UnitTestProject
 {
@@ -14,33 +17,76 @@ namespace UnitTestProject
         }
 
         [Test]
-        public void DeveriaRetornarUmItemSelecionadoParaDeletar()
+        public void DeveriaRetornarUmIdAoCriarNoBanco() //Create_InsereAcaoNoBancoDeDados_RetornaIdInteiro
         {
-            //ARRANGE
-            Mock<IProdutoFinanceiro> mockProdutoFinanceiro = new Mock<IProdutoFinanceiro>();
-            ProdutoFinanceiroViewModel sutProdutoFinanceiroViewModel = new ProdutoFinanceiroViewModel();
+            MySqlCRUD sutMySqlCrud = new MySqlCRUD();
 
-            //ACT
-            bool resultado = sutProdutoFinanceiroViewModel.DeleteProdutoFinanceiroCanUse(mockProdutoFinanceiro.Object);
+            string Sigla = "Teste";
+            string Nome = "Teste";
+            string Setor = "Teste";
+            string opcaoTabela = "produto_acao";
 
+            int resultado = sutMySqlCrud.Create(Sigla, Nome, Setor, opcaoTabela);
 
-            //ASSERT
-            Assert.IsTrue(resultado);
+            Assert.IsNotNull(resultado);
+
+            sutMySqlCrud.Delete(resultado, opcaoTabela);
         }
 
         [Test]
-        public void DeveriaRetornarUmItemSelecionadoParaEditar()
+        public void Delete_RemoveAcaoDoBancoDeDados_RetornaTrue()
         {
-            //ARRANGE
-            Mock<IProdutoFinanceiro> mockProdutoFinanceiro = new Mock<IProdutoFinanceiro>();
-            ProdutoFinanceiroViewModel sutProdutoFinanceiroViewModel = new ProdutoFinanceiroViewModel();
+            MySqlCRUD sutMySqlCrud = new MySqlCRUD();
 
-            //ACT
-            bool resultado = sutProdutoFinanceiroViewModel.UpdateProdutoFinanceiroCanUse(mockProdutoFinanceiro.Object);
+            string Sigla = "Teste";
+            string Nome = "Teste";
+            string Setor = "Teste";
+            string opcaoTabela = "produto_acao";
+            int id = sutMySqlCrud.Create(Sigla, Nome, Setor, opcaoTabela);
 
-            //ASSERT
-            Assert.IsTrue(resultado);
+            bool resultado = sutMySqlCrud.Delete(id, opcaoTabela);
+
+            Assert.IsNotNull(resultado);
+
         }
+
+        [Test]
+        public void AbrirConexao_AbreConexaoComBancoDeDados_RetornaUmaConexao()
+        {
+            DBConnection sutdbConnection = new DBConnection(new MySqlConnection());
+
+            var resultado = sutdbConnection.AbrirConexao();
+
+            Assert.IsTrue(resultado is IDbConnection);
+        }
+        //[Test]
+        //public void DeveriaRetornarUmItemSelecionadoParaDeletar()
+        //{
+        //    //ARRANGE
+        //    Mock<IProdutoFinanceiro> mockProdutoFinanceiro = new Mock<IProdutoFinanceiro>();
+        //    ProdutoFinanceiroViewModel sutProdutoFinanceiroViewModel = new ProdutoFinanceiroViewModel();
+
+        //    //ACT
+        //    bool resultado = sutProdutoFinanceiroViewModel.DeleteProdutoFinanceiroCanUse(mockProdutoFinanceiro.Object);
+
+
+        //    //ASSERT
+        //    Assert.IsTrue(resultado);
+        //}
+
+        //[Test]
+        //public void DeveriaRetornarUmItemSelecionadoParaEditar()
+        //{
+        //    //ARRANGE
+        //    Mock<IProdutoFinanceiro> mockProdutoFinanceiro = new Mock<IProdutoFinanceiro>();
+        //    ProdutoFinanceiroViewModel sutProdutoFinanceiroViewModel = new ProdutoFinanceiroViewModel();
+
+        //    //ACT
+        //    bool resultado = sutProdutoFinanceiroViewModel.UpdateProdutoFinanceiroCanUse(mockProdutoFinanceiro.Object);
+
+        //    //ASSERT
+        //    Assert.IsTrue(resultado);
+        //}
 
     }
 }
